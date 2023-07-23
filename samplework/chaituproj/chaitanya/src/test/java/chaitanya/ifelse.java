14e0da6c567ef6a597f5ecc332f8b1c3b6f7869c
Chaitanyaveeksha <140238676+Chaitanyaveeksha@users.noreply.github.com>
2023-07-23 14:09:21 +0530

    Add files via upload

---
+++ samplework/chaituproj/chaitanya/src/test/java/chaitanya/ifelse.java
@@ -0,0 +1,14 @@
+package chaitanya;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		 int age=20;  
+		    //checking the age  
+		    if(age>18){  
+		        System.out.print("Age is greater than 18");  
+	}
+
+
+	}
+}
